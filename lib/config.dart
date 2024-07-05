18b4da8f948b47f9d36635279e25755fa6430cea
Вадим <112276627+saythe0@users.noreply.github.com>
2024-07-05 12:06:51 +0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -2,5 +2,5 @@ class Config {
   static final Uri launcherJarUrl =
       Uri.parse('https://cdn.droplex.fun/launcher/Droplex.jar');
 
-  static final String projectDirectoryName = 'DroplexLauncher';
+  static final String projectDirectoryName = 'DroplexTest';
 }
